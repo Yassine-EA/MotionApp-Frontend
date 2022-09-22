065c10be445b3173f5b686200a2a81e95eca67ff
Yassine-EA <y.elaboubi@gmail.com>
2022-09-22 22:20:26 +0200

    #004

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,3 +1,4 @@
+import MyCarousel from '../../components/MyCarousel';
 import BaseLayout from '../../layouts/BaseLayout';
 
 
@@ -6,10 +7,12 @@ import BaseLayout from '../../layouts/BaseLayout';
 
 const Contact = () => {
     return (
-        <BaseLayout>
-            <h1>Contact</h1>
-
-        </BaseLayout>
+        <>
+            <BaseLayout>
+                <h1>Contact</h1>
+                <MyCarousel />
+            </BaseLayout>
+        </>
     );
 
 };
